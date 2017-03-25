b61a995cf2c6b2ffbbeacfb8382294c05be52fe7
Triwith Mutitakul <triwith.m@ku.th>
2017-03-26 02:59:09 +0700

    Update Stopwatch.java

--- src/stopwatch/Stopwatch.java
+++ src/stopwatch/Stopwatch.java
@@ -1,32 +1,27 @@
 package stopwatch;
 
 /**
- * A Stopwatch that measures elapsed time between a starting time and stopping
- * time, or until the present time.
+ * The class that use for measures elapsed time.
  * 
  * @author Triwith Mutitakul
  * @version 1.0
  */
 
 public class Stopwatch {
-	/** time that the stopwatch was started, in nanoseconds. */
-	long startTime;
-	/** time that the stopwatch was stopped, in nanoseconds. */
-	long stopTime;
-	/** variable for show you that the stopwatch is running or not. */
-	boolean running;
-	/** constant for converting nanoseconds to seconds. */
+	private long startTime;
+	private long stopTime;
+	private boolean running;
 	private static final double NANOSECONDS = 1.0E-9;
 
 	/**
-	 * the contructor of stopwatch.
+	 * The contructor of stopwatch.
 	 */
 	public Stopwatch() {
 
 	}
 
 	/**
-	 * Start the stopwatch if it is not already running.
+	 * This me thod is use for start the stopwatch.
 	 */
 	public void start() {
 		if (!isRunning()) {
@@ -36,7 +31,7 @@ public class Stopwatch {
 	}
 
 	/**
-	 * Stop the stopwatch if it is not already running.
+	 * This method is use for stop the stopwatch.
 	 */
 	public void stop() {
 		if (isRunning()) {
@@ -48,11 +43,9 @@ public class Stopwatch {
 
 	/**
 	 * 
-	 * return the elapsed time.
+	 * This method is use for return the elapsed time.
 	 * 
-	 * @return if the stopwatch is still running return the elapsed time until
-	 *         now,if the stopwatch is stop return the elapsed time between
-	 *         start and stop.
+	 * @return return the elapsed time.
 	 */
 	public double getElapsed() {
 		if (isRunning()) {
@@ -63,10 +56,9 @@ public class Stopwatch {
 	}
 
 	/**
-	 * check the stopwatch is running or not.
+	 * This method is use for check the stopwatch is running or not.
 	 * 
-	 * @return true if stopwatch is running and return false if stopwatch is
-	 *         stop.
+	 * @return true if stopwatch is running,return false if stopwatch is stop.
 	 */
 	public boolean isRunning() {
 		return running;
